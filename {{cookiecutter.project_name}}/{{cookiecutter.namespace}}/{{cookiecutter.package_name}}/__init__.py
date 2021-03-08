700b4e3cf5651b4161a213052dd6c4ebebf3be45
Christian Klinger <ck@novareto.de>
2021-03-08 14:06:24 +0100

    working on layout

--- {{cookiecutter.project_name}}/{{cookiecutter.namespace}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.namespace}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,1 +1,6 @@
-# Package
+import pathlib
+from reiter.application.browser import TemplateLoader
+
+
+TEMPLATES = TemplateLoader(
+    str((pathlib.Path(__file__).parent / "templates").resolve()), ".pt")
